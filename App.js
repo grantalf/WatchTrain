513aeb196f71ce85d612cabcd4b0d3e3a01c709a
Grant Harris <grantxvx@gmail.com>
2020-10-28 21:25:15 -0500

    formatted buttons and added lap table

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import moment from 'moment';
 
 const DATA = {
@@ -21,8 +21,39 @@ function Timer({interval}) {
 function RoundButton({title, color, background}) {
   return (
     <View style={[styles.button, {backgroundColor: background}]}>
+      <View style={styles.buttonBorder}>
       <Text style={[styles.buttonTitle, {color}]}>{title}</Text>
+      </View>
     </View>
+  )
+}
+
+function Lap({number, interval}) {
+  return (
+    <View style={styles.lap}>
+      <Text style={styles.lapText}>Lap {number}</Text>
+      <Text style={styles.lapText}>{interval}</Text>
+    </View>
+  )
+}
+
+function LapsTable({laps}) {
+  return (
+    <ScrollView style={styles.scrollView}>
+      {laps.map((lap, index, key) => (
+        <Lap
+          key={laps.length - index}
+          number={laps.length - index}
+          interval={lap}
+        />
+      ))}
+    </ScrollView>
+  )
+}
+
+function ButtonsRow({children}) {
+  return (
+    <View style={styles.buttonsRow}>{children}</View>
   )
 }
 
@@ -30,7 +61,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Timer interval={DATA.timer} />
-      <RoundButton title='Start'color='#50D167' background='#1B361F'/>
+      <ButtonsRow>
+        <RoundButton title='Reset'color='#FFFFFF' background='#3D3D3D'/>
+        <RoundButton title='Start'color='#50D167' background='#1B361F'/>
+      </ButtonsRow>
+      <LapsTable laps={DATA.laps} />
     </View>
   );
 }
@@ -41,6 +76,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#0D0D0D',
     alignItems: 'center',
     paddingTop: 130,
+    paddingHorizontal: 30,
   },
   timer: {
     color: '#FFFFFF',
@@ -56,5 +92,32 @@ const styles = StyleSheet.create({
   },
   buttonTitle: {
     fontSize: 19
+  },
+  buttonBorder: {
+    width: 75,
+    height: 75,
+    borderRadius: 38,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonsRow: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    marginTop: 80,
+  },
+  lapText: {
+    color: '#FFFFFF',
+    fontSize: 19,
+  },
+  lap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  scrollView: {
+    alignSelf: 'stretch',
+    paddingTop: 20,
+    paddingHorizontal: 15,
   }
 });
