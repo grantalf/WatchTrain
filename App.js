9af42d071575736cd585482d3d7e3115844f0e46
Grant Harris <grantxvx@gmail.com>
2020-10-28 20:41:07 -0500

    fleshing out App with dummy data

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import moment from 'moment';
+
+const DATA = {
+  timer: 1234567,
+  laps: [12345, 2345, 2312, 39093]
+}
+
+function Timer({interval}) {
+  const duration = moment.duration(interval);
+  return (
+    <Text style={styles.timer}>
+      {duration.minutes()}:{duration.seconds()}.{duration.milliseconds()}
+    </Text>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Timer interval={DATA.timer} />
     </View>
   );
 }
@@ -14,8 +28,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0D0D0D',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 130,
   },
+  timer: {
+    color: '#FFFFFF',
+    fontSize: 76,
+    fontWeight: '200'
+  }
 });
